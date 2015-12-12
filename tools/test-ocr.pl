6a17780af9db9d8370a10627a47a2faed4c2c88a
Takashi Okumura <taka@wide.ad.jp>
2015-12-13 01:46:06 +0900

    fixed a bug that misrecognizes the outpuf of kocr results.

--- tools/test-ocr.pl
+++ tools/test-ocr.pl
@@ -4,24 +4,33 @@ use utf8;
 
 my $count = 0;
 my $success = 0;
-my $KOCR_PATH = "/home/faxocr/src/kocr/src/kocr";
-my $DB_PATH = "/home/faxocr/src/kocr/list-num.xml";
+my $KOCR_PATH = "/home/devel/faxocr/src/kocr/src/kocr";
+my $DB_PATH = "/home/devel/faxocr/src/kocr/list-num.xml";
+# my $DB_PATH = "/home/devel/faxocr/src/kocr/databases/list-num.xml";
 
 foreach (@ARGV) {
     $filepath = $_;
-    $filepath =~ /([^\/]+)$/;
 
-    $filename = $1;
-    $filename =~ /^(\d)-/;
-
-    $num_label = $1;
-    $count++;
+    if ($filepath =~ /([^\/]+)$/) {
+	$filename = $1;
+	if ($filename =~ /^(\d)-/) {
+	    $num_label = $1;
+	} else {
+	    next;
+	}
+	$count++;
+    } else {
+	next;
+    }
 
     $cmd = $KOCR_PATH . " " . $DB_PATH . " " . $filepath;
     $output = qx/$cmd/;
 
-    $output =~ /Result:\s+(\d+)/;
-    $result = $1;
+    if ($output =~ /Result:\s+(\d+)/) {
+	$result = $1;
+    } else {
+	$result = "N/A";
+    }
     
     print $filename . "\t" . $num_label . "\t" . $result . "\n";
 
