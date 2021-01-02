1ad7b9014664ebe1f7b307558cf1e0863a9f73a0
Sohgo Takeuchi <sohgo@sohgo.more.jp>
2021-01-02 18:37:06 +0900

    Make command name of python and pip configurable

--- learning/install_packages.sh
+++ learning/install_packages.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+PIP=${PIP:-pip}
+PYTHON=${PYTHON:-python}
+
 apt-get update -y
 apt-get install python-dev libblas-dev -y
 
-pip install Pillow==5.0.0
-pip install numpy==1.14.1
-pip install scipy==1.0.0
-pip install h5py==2.7.1
-pip install Theano==1.0.1
-pip install Keras==2.1.4
+"$PIP" install Pillow==5.0.0
+"$PIP" install numpy==1.14.1
+"$PIP" install scipy==1.0.0
+"$PIP" install h5py==2.7.1
+"$PIP" install Theano==1.0.1
+"$PIP" install Keras==2.1.4
 
-python -c "import keras" 2>/dev/null
+"$PYTHON" -c "import keras" 2>/dev/null
 
 sed -i -e s/\"channels_last\"/\"channels_first\"/ ~/.keras/keras.json
 sed -i -e s/\"tensorflow\"/\"theano\"/ ~/.keras/keras.json
